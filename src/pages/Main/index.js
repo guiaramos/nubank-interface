81a4dd650221c8fb10f01e9930cb358c455647c6
Guilherme de Abreu Ramos <Gui@MacBook-Pro.local>
2019-09-08 12:42:51 +0900

    Add: Main card

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,13 +1,40 @@
 import React from 'react';
-import {View} from 'react-native';
-import {Container} from './styles';
+import {
+  Container,
+  Content,
+  Card,
+  CardHeader,
+  CardContent,
+  CardFooter,
+  Title,
+  Description,
+  Annotation,
+} from './styles';
 import Header from '~/components/Header';
 import Tabs from '~/components/Tabs';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 export default function Main() {
   return (
     <Container>
       <Header />
+      <Content>
+        <Card>
+          <CardHeader>
+            <Icon name="attach-money" size={28} color="#666" />
+            <Icon name="visibility-off" size={28} color="#666" />
+          </CardHeader>
+          <CardContent>
+            <Title>Amount available</Title>
+            <Description>US$ 420,789,19</Description>
+          </CardContent>
+          <CardFooter>
+            <Annotation>
+              Received transfer of US$ 20,000 from Mark Zulch today at 8:00am.
+            </Annotation>
+          </CardFooter>
+        </Card>
+      </Content>
       <Tabs />
     </Container>
   );
